2fb53360946acb7e1309f2d243e6b37d3f90ab66
9areeba <areeba-bito@hotmail.com>
2022-10-03 18:58:47 +0100

    Add code to count frequencies

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,19 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from pip import main
+
+
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if type(item) == int:
+            item = str(item)
+            
+        if item in frequencies:
+            frequencies[item] = frequencies.get(item) + 1
+        else:
+            frequencies[str(item)] = 1
+
     return frequencies
